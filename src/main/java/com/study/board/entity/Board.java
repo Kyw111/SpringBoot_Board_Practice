408963cffae5253dc228454f6890c423f4f0798e
YOUNGWOO KIM <partan5260@naver.com>
2022-03-03 18:15:21 +0900

    Initial commit

---
+++ src/main/java/com/study/board/entity/Board.java
@@ -0,0 +1,27 @@
+package com.study.board.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+}
---
+++ src/main/java/com/study/board/service/BoardService.java
@@ -0,0 +1,36 @@
+package com.study.board.service;
+
+import com.study.board.entity.Board;
+import com.study.board.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BoardService {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    //새글 작성
+    public void Write(Board board) {
+        boardRepository.save(board);
+    }
+
+    //게시글 리스트
+    public List<Board> boardList() {
+        return boardRepository.findAll();
+    }
+
+    //특정 게시글 조회 - 클래스를 Board로 해줘야 함
+    public Board boardView(Integer id){
+        return boardRepository.findById(id).get();
+    }
+    //특정 게시글 삭제
+    public void boardDelete(Integer id){
+        boardRepository.deleteById(id);
+    }
+}
